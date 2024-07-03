f3eca4ad5b1fe836a4ecee78aff53f8181d1081c
ayushrai <63505520+ayushrai-bhau@users.noreply.github.com>
2024-07-04 00:02:39 +0530

    @@

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./Config/db');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
